f3af3e692016e629a0ce1b472a812fa9c58a3e3c
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2026-02-04 11:22:45 +0100

    feat: added navbar component, project colors and texts, modified other components

--- nodejs/frontend/src/main.js
+++ nodejs/frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import '@/assets/base.css'
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
--- nodejs/frontend/src/router/index.js
+++ nodejs/frontend/src/router/index.js
@@ -1,14 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '@/views/Home.vue';
+import Map from '@/views/Map.vue';
+import Inspiration from '@/views/Inspiration.vue';
+import MyMaps from '@/views/MyMaps.vue';
+import Profile from '@/views/Profile.vue';
 
 const routes = [
-    { path: '/', redirect: '/home' },
+    { path: '/', redirect: '/map' },
     {
-        path: '/home',
-        name: "Home",
-        component: Home,
-        meta: { requiresAuth: true, requiresAdmin: false, showSidebar: true, activeMenu: 'home' }
+        path: '/map',
+        name: "Map",
+        component: Map,
+        meta: { activeMenu: 'map' }
+    },
+    {
+        path: '/inspiration',
+        name: "Inspiration",
+        component: Inspiration,
+        meta: { activeMenu: 'inspiration' }
+    },
+    {
+        path: '/my-maps',
+        name: "MyMaps",
+        component: MyMaps,
+        meta: { activeMenu: 'my-maps' }
+    },
+    {
+        path: '/profile',
+        name: "Profile",
+        component: Profile,
+        meta: { activeMenu: 'profile' }
     }
 ];
 
