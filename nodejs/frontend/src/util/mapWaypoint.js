4ddc5e2bdda2886edd1ee23ffbcfbdcb002dfd4c
Nicolas Amadori <nicolas.amadori02@gmail.com>
2026-02-05 12:03:56 +0100

    feat: added bubbleicon and video embedding

--- nodejs/frontend/src/util/mapWaypoint.js
+++ nodejs/frontend/src/util/mapWaypoint.js
@@ -8,3 +8,23 @@ export const htmlMarkerIcon = (icon = 'geo', color = '#555555', isMustHave = fal
         iconAnchor: [size / 2, size / 2],
     });
 };
+
+export const bubbleIcon = (size = 12) => {
+    const pixelSize = size * 4;
+
+    return L.divIcon({
+        html: `
+            <div class="relative flex items-center justify-center bg-bright shadow-lg" 
+                 style="width: ${pixelSize}px; height: ${pixelSize}px; 
+                        border-radius: 50% 50% 50% 0; 
+                        transform: rotate(-45deg);">
+                
+                <i class="bi bi-file-play text-white flex items-center justify-center" 
+                   style="transform: rotate(45deg); font-size: ${pixelSize / 2}px;">
+                </i>
+            </div>`,
+        className: '',
+        iconSize: [pixelSize, pixelSize],
+        iconAnchor: [0, pixelSize], 
+    });
+};
